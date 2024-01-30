e3f9e0e507425b104e61a4862970a0d8a224856b
nickhealthy <alskadmlcraz1@gmail.com>
2024-01-30 17:24:49 +0900

    스프링 MVC - 웹 페이지 만들기 | 상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -9,9 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -47,10 +49,56 @@ public class BasicItemController {
     }
 
     /* 상품 등록 폼 */
-    @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+//    @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model
+    ) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
     }
+
+    /* 상품 등록 폼 */
+//    @PostMapping("/add")
+    public String addItemV2(
+            @ModelAttribute("item") Item item,
+            Model model
+    ) {
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    /* 상품 등록 폼
+    * @ModelAttribute의 이름을 생략하면 모델에 저장될 때 클래스명을 사용한다.
+    * 이때 클래스의 첫 글자만 소문자로 변경해서 등록한다.
+    *  */
+//    @PostMapping("/add")
+    public String addItemV3(
+            @ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /* 상품 등록 폼 */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
 
     /**
      * 테스트용 데이터 추가
